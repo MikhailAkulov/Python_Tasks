16453e2ff7c11743d83cfaf05504b61c4903dec3
Akuloff.M <Akuloff.M@gmail.com>
2022-05-12 00:20:08 +0300

    add Task024_1 project

---
+++ Task024_1.py
@@ -0,0 +1,23 @@
+# Задача 24. В заданном списке вещественных чисел найдите разницу между максимальным и минимальным значением дробной части элементов. 
+# Пример: [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+
+import os
+os.system("cls")
+
+max = 0
+min = 1
+
+list_of_numbers = [1.1, 1.2, 3.1, 5, 10.01]
+print(f'На входе имеем список чисел:\n{list_of_numbers}')
+
+for element in list_of_numbers:
+    swap = str(float(element)).split('.')
+    if int(swap[1]):
+        fraction = int(swap[1])/(10**len(swap[1]))
+        if fraction < min:
+            min = fraction
+        if fraction > max:
+            max = fraction
+
+print(
+    f'Максимальное значение дробной части: {max},\nминимальное значение дробной части: {min}\nразница между максимальной и минимальной частями равна: {max-min}')
