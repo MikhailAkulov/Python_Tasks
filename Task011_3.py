b04bbc83ae774afabce730ebb77224f3c70108cc
Akuloff.M <Akuloff.M@gmail.com>
2022-05-12 00:11:30 +0300

    add Task011_3 project

---
+++ Task011_3.py
@@ -0,0 +1,15 @@
+# Задача 11. Сформировать список из  N членов последовательности.
+# Для N = 5: 1, -3, 9, -27, 81 и т.д.
+
+import os
+os.system("cls")
+
+count = int(input('Введите количество элементов: '))
+num = 1
+list = [1]
+
+for i in range (0, count - 1):
+    num *= -3
+    list.append(num)
+
+print(list)
