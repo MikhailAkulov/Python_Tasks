a09cd9cbc29db739e0bbb427fe01b618ff45cc9a
Akuloff.M <Akuloff.M@gmail.com>
2022-05-30 02:56:45 +0300

    add Task040 project

--- Task040_3.py
+++ Task040_3.py
@@ -3,7 +3,7 @@
 import os
 os.system("cls")
 
-print("*" * 10, " Игра Крестики-нолики для двух игроков ", "*" * 10)
+print('Это игра крестики-нолики')
 
 board = list(range(1,10))
 
@@ -13,23 +13,23 @@ def draw_board(board):
       print("|", board[0+i*3], "|", board[1+i*3], "|", board[2+i*3], "|")
       print("-" * 13)
 
-def take_input(player_token):
+def take_input(player_symbol):
    valid = False
    while not valid:
-      player_answer = input("Куда поставим " + player_token+"? ")
+      player_answer = input("Укажите номер ячейки, куда поставим " + player_symbol+"? ")
       try:
          player_answer = int(player_answer)
       except:
-         print("Некорректный ввод. Вы уверены, что ввели число?")
+         print("Ошибка ввода! Введите число")
          continue
       if player_answer >= 1 and player_answer <= 9:
          if(str(board[player_answer-1]) not in "XO"):
-            board[player_answer-1] = player_token
+            board[player_answer-1] = player_symbol
             valid = True
          else:
             print("Эта клетка уже занята!")
       else:
-        print("Некорректный ввод. Введите число от 1 до 9.")
+        print("Ошибка ввода! Введите число от 1 до 9.")
 
 def check_win(board):
    win_coord = ((0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6))
